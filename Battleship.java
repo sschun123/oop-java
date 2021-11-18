454d63345b800d6f8171e0ee3692d0ffc93bc946
Sam Chun <schun@auditboard.com>
2021-11-17 17:41:08 -0800

    Battleship * Add core game logic * Add console printing for game results

--- Battleship.java
+++ Battleship.java
@@ -9,6 +9,7 @@ public class Battleship {
 
     static final protected int MAX_PLAYERS = 2;
     static final protected int MAX_SHIPS = 5;
+    static final protected int BOARD_SIZE = 5;
 
     static protected boolean gameActive = true;
 
@@ -33,25 +34,100 @@ public class Battleship {
         Scanner input = new Scanner(System.in);
 
         Battleship.initGame(input);
-
-        // do {
-        //     int[] playerOneCoordinates = Battleship.getCoordinates();
-
-        // } while (Battleship.gameActive);
+        Battleship.playGame("1", Battleship.playerOneBoard, Battleship.playerTwoBoard, input);
     }
 
-    // private static int[] getCoordinates() {
-        
-    // }
+    private static void playGame(String player, char[][] boardOne, char[][] boardTwo, Scanner input) {
+        boolean gameover = false;
+
+        do {
+            Battleship.playTurn("1", "2", boardTwo, input);
+            Battleship.printBattleShip(boardTwo);
+            int boardTwoHits = Battleship.countHits(boardTwo);
+            if (boardTwoHits == Battleship.MAX_SHIPS) {
+                gameover = true;
+                break;
+            }
+            Battleship.playTurn("2", "1", boardOne, input);
+            Battleship.printBattleShip(boardOne);
+            int boardOneHits = Battleship.countHits(boardOne);
+            if (boardOneHits == Battleship.MAX_SHIPS) {
+                gameover = true;
+                break;
+            }
+        } while (!gameover);
+    }
+
+    private static void playTurn(String playerName, String oppName, char[][] board, Scanner input) {
+        boolean validInput = false;
+
+        int x = 0;
+        int y = 0;
+        do {
+            System.out.printf("Player %s, enter hit row/column:\n", playerName);
+            x = input.nextInt();
+            y = input.nextInt();
+            if (x >= Battleship.BOARD_SIZE) {
+                System.out.println("Invalid coordinates. Choose different coordinates.");
+            }
+            else if (board[x][y] == Battleship.HIT || board[x][y] == Battleship.MISS) {
+                System.out.println("You already fired on this spot. Choose diffferent coordinates.");
+            }
+            else {
+                validInput = true;
+            }
+        } while (!validInput);
+
+        if (board[x][y] == Battleship.SHIP) {
+            board[x][y] = Battleship.HIT;
+            System.out.printf("PLAYER %s HIT PLAYER %s's SHIP!\n", playerName, oppName);
+        }
+        else if (board[x][y] == Battleship.EMPTY) {
+            board[x][y] = Battleship.MISS;
+            System.out.printf("PLAYER %s MISSED!\n", playerName);
+        }
+
+        int hits = Battleship.countHits(board);
+        if (hits >= Battleship.MAX_SHIPS) {
+            System.out.printf("PLAYER %s WINS! YOU SUNK ALL OF OUR OPPONENT'S SHIPS!\n", playerName);
+            return;
+        }
+    }
 
     private static int countHits(char[][] board) {
-        return 3;
+        int hits = 0;
+        for (int row = 0; row < board.length; row++) {
+            for (int column = 0; column < board[row].length; column++) {
+                if (board[row][column] == Battleship.HIT) {
+                    hits++;
+                }
+                if (hits >= Battleship.MAX_SHIPS) {
+                    break;
+                }
+            }
+        }
+        return hits;
     }
 
     private static void initGame(Scanner input) {
+        char[][] boardOne = Battleship.initBoard("PLAYER 1", Battleship.playerOneBoard, input);
+        printBattleShip(boardOne);
+        Battleship.printLines(100);
+        char[][] boardTwo = Battleship.initBoard("PLAYER 2", Battleship.playerTwoBoard, input);
+        printBattleShip(boardTwo);
+        Battleship.printLines(100);
+    }
+
+    private static void printLines(int lines) {
+        for (int i = 0; i < lines; i++) {
+            System.out.println("");
+        }
+    }
+
+    private static  char[][] initBoard(String playerName, char[][] board, Scanner input) {
+        System.out.printf("%s, ENTER YOUR SHIPS' COORDINATES\n", playerName);
         for (int j = 0; j < MAX_SHIPS; j++) {
             boolean validInput = false;
-            System.out.println("PLAYER X, ENTER YOUR SHIPS' COORDINATES");
             do {
                 System.out.printf("Enter ship %d location:\n", j + 1);
 
@@ -62,27 +138,19 @@ public class Battleship {
                     if (row >= MAX_SHIPS || column >= MAX_SHIPS) {
                         System.out.println("Invalid coordinates. Choose different coordinates.");
                     }
-                    else if (Battleship.playerOneBoard[row][column] == Battleship.SHIP) {
+                    else if (board[row][column] == Battleship.SHIP) {
                         System.out.println("You already have a ship there. Choose different coordinates.");
                     }
                     else {
                         validInput = true;
-                        Battleship.playerOneBoard[row][column] = Battleship.SHIP;
+                        board[row][column] = Battleship.SHIP;
                     }
                 } catch (InputMismatchException e) {
                     System.out.println("Invalid coordinates. Choose different coordinates.");
                 }
             } while (!validInput);
         }
-        printBattleShip(playerOneBoard);
-        // for loop 2 players
-            // for loop 5 ships
-                // prompt for ship coordinates ex: "1 5"
-                // error invalid coordinates "Invalid coordinates. Choose different coordinates."
-                // error coordinates are occupied "You already have a ship there. Choose different coordinates."
-                // addShip
-            // printBattleShip
-            // print 100 new lines
+        return board;
     }
 
     // Use this method to print game boards to the console.
